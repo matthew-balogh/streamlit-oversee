acd084313eafeae766b920251dc5c2ee02128c6c
Matthew Balogh <meld-03-spouts@icloud.com>
2025-08-18 01:54:08 +0200

    Add "What's new" tab under help page

---
+++ pages/contents/whatsnew.md
@@ -0,0 +1,15 @@
+### Introducing Vessels
+<small>2025.08.18</small>
+
+From now on, *Vessels* represent projects that enclose *manuscripts*. 
+
+For now, Oversee only supports a single project, therefore you cannot have more than one vessel. Nevertheless, the introduction of vessels brings a clarity on what you are working on with your manuscripts. Simply state the title and description of your project in `details.json` and `description.md` under `storage/vessel` and Oversee automatically updates the view to reflect your project details.
+
+If you want to work on a new separate project, we recommend to fork/clone a new Oversee repository and name it accordingly.
+
+<br>
+
+### Oversee is out!
+<small>2025.08.17</small>
+
+*Oversee* is published and available to fork from *GitHub* and *Streamlit*.
--- pages/help.py
+++ pages/help.py
@@ -1,10 +1,20 @@
 import streamlit as st
+import os
+
+WHATSNEW_FILEPATH = f"pages/contents/whatsnew.md"
 
 st.subheader(":material/help: Help")
 
-tab1, tab2 = st.tabs(["Getting started", "Manuscript indicators"])
+tab1, tab2, tab3 = st.tabs(["What's new on the Horizon", "Getting started", "Manuscript indicators"])
 
 with tab1:
+    if os.path.exists(WHATSNEW_FILEPATH):
+        with open(WHATSNEW_FILEPATH, "r") as f:
+            content = f.read()
+            f.close()
+        st.markdown(content, unsafe_allow_html=True)
+
+with tab2:
     st.warning("Limitation notice", icon=":material/warning:")
     st.write("While this demo gives you a quick feel for the tool, you can enjoy all its features by cloning the repository and running it locally or forking this streamlit app and managing in GitHub Codespaces online.")
     st.write("By doing so, you will be able to directly modify the contents of different elements of a _manuscript_ such as the lab file, notes, results and future directions, along with creating new manuscripts.")
@@ -12,7 +22,7 @@ with tab1:
     st.write("For the detailed instructions to setup **Oversee**, visit the GitHub repository:")
     st.link_button("Go to repository", "https://github.com/matthew-balogh/streamlit-oversee", type="primary", icon=":material/folder_data:")
 
-with tab2:
+with tab3:
     with st.container(horizontal=True, vertical_alignment="center"):
         st.button("", key="help:indicator:research_results", icon=":material/lab_panel:", type="tertiary", disabled=True)
         st.write("This icon indicates that research results have been obtained for the manuscript.")
