78501698ac2b1dff94a9ff68ebfc2a9b8c358d37
Matthew Balogh <meld-03-spouts@icloud.com>
2025-08-17 17:21:10 +0200

    Add home message

---
+++ pages/home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.header("Welcome to the Harbor!")
+
+st.warning("The harbor is under construction. Please return later to dock.", icon=":material/front_loader:")
+st.info("While the harbor is being built, enjoy exploring overseas, but stay within reach.", icon=":material/sailing:")
