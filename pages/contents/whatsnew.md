d4d2da4905d846a4e8225d7ffe2f9269fce0f9ee
Matthew Balogh <meld-03-spouts@icloud.com>
2025-08-23 17:06:40 +0200

    Update help page

--- pages/contents/whatsnew.md
+++ pages/contents/whatsnew.md
@@ -1,14 +1,38 @@
+### Introducing Anchors
+
+<small>2025.08.23</small>
+
+*Take a pause.*
+
+Drop an anchor to slow down and reset. Use the **Anchor down!** button whenever you need a moment to reflect before continuing with a clearer mind.
+
+<br>
+
+### Introducing Dives
+
+<small>2025.08.23</small>
+
+*Don't know where to start? Don't worry about where to sail, just dive in!*
+
+Use the **Dive!** button to capture your first thoughts, then shape them into a manuscript or link them to one you’re already working on.
+
+<br>
+
 ### Create Manuscripts with ease
+
 <small>2025.08.19</small>
 
 You can now create new manuscripts by clicking on the **Write new Manuscript** button on the Manuscripts page.
 
 > ⚠️ Warning: To prevent this feature from breaking, do not modify any files in the `_templates` folder.
 
+<br>
+
 ### Introducing Vessels
+
 <small>2025.08.18</small>
 
-From now on, *Vessels* represent projects that enclose *manuscripts*. 
+From now on, *Vessels* represent projects that enclose *manuscripts*.
 
 For now, Oversee only supports a single project, therefore you cannot have more than one vessel. Nevertheless, the introduction of vessels brings a clarity on what you are working on with your manuscripts. Simply state the title and description of your project in `details.json` and `description.md` under `storage/vessel` and Oversee automatically updates the view to reflect your project details.
 
@@ -16,7 +40,8 @@ If you want to work on a new separate project, we recommend to clone a new Overs
 
 <br>
 
-### Oversee is out!
+### Oversee is out
+
 <small>2025.08.17</small>
 
 *Oversee* is published and available to fork and clone from *GitHub* and *Streamlit*.
--- pages/help.py
+++ pages/help.py
@@ -63,83 +63,102 @@ with tab_intro:
 
     with st.chat_message("assistant"):
         st.write("Hi! It's me your personal assistant. How can I help you today?")
-        time.sleep(1.5)
-
-    with st.chat_message("user"):
-        st.write("I would like to organize my **\"Introduction to Data Science\"** subject experiments, while tracking it in git. I work with notebooks, write notes and sometimes jot down my thoughts on my computer. I want to focus on specific problems in isolated environments while being able to oversee the whole progress.")
-        time.sleep(2.5)
-
-    with st.chat_message("assistant"):
-        st.write("Then, you may try **Oversee**! Would you like me to set it up for you?")
-        time.sleep(2)
-
-    with st.chat_message("user"):
-        st.write("Sure!")
-        time.sleep(1.5)
-
-    with st.chat_message("assistant"):
-        with st.status("Creating your Oversee project...", expanded=True) as status:
-            s = st.empty()
-            s.write("⏳ Cloning `streamlit-oversee` from GitHub")
-            time.sleep(3)
-            s.write("✔️ Cloned `streamlit-oversee` from GitHub")
-
-            s = st.empty()
-            s.write("⏳ Renaming to `oversee-intro-to-data-science`")
-            time.sleep(1)
-            s.write("✔️ Renamed to `oversee-intro-to-data-science`")
-
-            s = st.empty()
-            s.write("⏳ Installing dependencies")
-            time.sleep(3)
-            s.write("✔️ Installed dependencies")
-
-            s = st.empty()
-            s.write("⏳ Starting your app")
-            time.sleep(2)
-            s.write("✔️ Started your app")
-
-            s = st.empty()
-            s.write("🎈🎉  Your Oversee app is available at `http://localhost:1234`")
-
-            status.update(
-                label="Oversee is now set up for your project!", state="complete", expanded=True
-            )
     
-    time.sleep(3)
-
     with st.chat_message("user"):
-        st.write("That looks great! I think I'd like to *oversee* my **\"Master's Thesis\"** project in a similar environment.")
-        time.sleep(1.5)
+        st.write("I work with notebooks, write notes and sometimes jot down my thoughts on my computer. I want to focus on specific problems in isolated environments while being able to oversee the whole progress.")
+    
+    placeholder = st.empty()
 
-    with st.chat_message("assistant"):
-        st.write("Say no more! I've got you covered!")
-        time.sleep(2)
+    with placeholder.container():
+        with st.chat_message("user"):
+            st.write("I would like to oversee my...")
 
-        with st.status("Creating another Oversee project for your Master's Thesis...", expanded=True) as status:
-            s = st.empty()
-            s.write("⏳ Cloning `streamlit-oversee` from GitHub")
-            time.sleep(2)
-            s.write("✔️ Cloned `streamlit-oversee` from GitHub")
+        with st.container(horizontal=True):
+            option1 = "**\"Introduction to Data Science\"** subject experiments"
+            option2 = "**\"Master's Thesis\"** project"
 
-            s = st.empty()
-            s.write("⏳ Renaming to `oversee-masters-thesis`")
+            button1 = st.button(option1, type="secondary")
+            button2 = st.button(option2, type="secondary")
+
+            answer1 = f"I would like to oversee my {option1}."
+            answer2 = f"I would like to oversee my {option2}."
+
+    if button1:
+        placeholder.empty()
+
+        with st.chat_message("user"):
+            st.write(answer1)
+            time.sleep(.5)
+
+        with st.chat_message("assistant"):
+            st.write("Let me fetch *Oversee* for you...")
             time.sleep(1)
-            s.write("✔️ Renamed to `oversee-masters-thesis`")
 
-            s = st.empty()
-            s.write("⏳ Installing dependencies")
+        with st.chat_message("assistant"):
+            with st.status("Creating your Oversee project...", expanded=True) as status:
+                s = st.empty()
+                s.write("⏳ Cloning `streamlit-oversee` from GitHub")
+                time.sleep(3)
+                s.write("✔️ Cloned `streamlit-oversee` from GitHub")
+
+                s = st.empty()
+                s.write("⏳ Renaming to `oversee-intro-to-data-science`")
+                time.sleep(1)
+                s.write("✔️ Renamed to `oversee-intro-to-data-science`")
+
+                s = st.empty()
+                s.write("⏳ Installing dependencies")
+                time.sleep(3)
+                s.write("✔️ Installed dependencies")
+
+                s = st.empty()
+                s.write("⏳ Starting your app")
+                time.sleep(2)
+                s.write("✔️ Started your app")
+
+                s = st.empty()
+                s.write("🎈🎉  Your Oversee app is available at `http://localhost:1234`")
+
+                status.update(
+                    label="Oversee is now set up for your project!", state="complete", expanded=True
+                )
+
+    if button2:
+        placeholder.empty()
+
+        with st.chat_message("user"):
+            st.write(answer2)
+            time.sleep(.5)
+
+        with st.chat_message("assistant"):
+            st.write("Say no more! I've got you covered!")
             time.sleep(1)
-            s.write("✔️ Installed dependencies")
 
-            s = st.empty()
-            s.write("⏳ Starting your app")
-            time.sleep(1)
-            s.write("✔️ Started your app")
+        with st.chat_message("assistant"):
+            with st.status("Creating Oversee project for your Master's Thesis...", expanded=True) as status:
+                s = st.empty()
+                s.write("⏳ Cloning `streamlit-oversee` from GitHub")
+                time.sleep(3)
+                s.write("✔️ Cloned `streamlit-oversee` from GitHub")
 
-            s = st.empty()
-            s.write("🎈🎉  Your Oversee app is available at `http://localhost:1235`")
+                s = st.empty()
+                s.write("⏳ Renaming to `oversee-masters-thesis`")
+                time.sleep(1)
+                s.write("✔️ Renamed to `oversee-masters-thesis`")
 
-            status.update(
-                label="Oversee is now set up for your Master's Thesis project!", state="complete", expanded=True
-            )
+                s = st.empty()
+                s.write("⏳ Installing dependencies")
+                time.sleep(3)
+                s.write("✔️ Installed dependencies")
+
+                s = st.empty()
+                s.write("⏳ Starting your app")
+                time.sleep(2)
+                s.write("✔️ Started your app")
+
+                s = st.empty()
+                s.write("🎈🎉  Your Oversee app is available at `http://localhost:1235`")
+
+                status.update(
+                    label="Oversee is now set up for your Master's Thesis project!", state="complete", expanded=True
+                )
